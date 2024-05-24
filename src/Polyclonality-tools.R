327525950380134772827c88cad17765d6aade78
Arnaud Le Rouzic <lerouzic@egce.cnrs-gif.fr>
2024-05-24 17:21:41 +0200

    Switching to a mle implementation where p and muC are estimated simultaneously.

--- src/Polyclonality-tools.R
+++ src/Polyclonality-tools.R
@@ -1,6 +1,10 @@
+library(stats4)
+library(gtools)
+library(multicool)
+
 
 ############################## Constants ###############################
-max.nbclones <- 7
+max.nbclones <- 15
 
 col.names <-  c("G","R","Y","B")
 colors    <-  c(G="green", R="darkred", Y="yellow", B="turquoise")
@@ -49,55 +53,60 @@ obs2p <- function(obs) {
 
 ############################ Model fitting #############################
 
-cache.gridcat <- lapply(seq_len(max.nbclones), function(cl) apply(expand.grid(replicate(cl, col.names, simplify=FALSE)), 1, function(x) paste0(unique(x[order(match(x, col.names))]), collapse="")))
+cache.cat <- lapply(1:max.nbclones, function(cl) { 
+	# Pre-calculated and stored in a variable to save time.
+		pm <- gtools::combinations(4, cl, rev(col.names), set=FALSE, repeats.allowed=TRUE)
+		list(
+			comb = pm, 
+			choose = apply(pm, 1, multicool::multinom), 
+			names = apply(pm, 1, function(x) paste0(unique(x[order(match(x, col.names))]), collapse=""))
+		)
+	})
 
-# Nomenclature:
-#
-# exact  : exactly nbclones clones
-# random : average meanclones clones (Poisson distribution)
 
-freq.model.exact <- function(p, nbclones) {
-	ref.nn <- cache.gridcat[[nbclones]]
-	prbs <- setNames(apply(expand.grid(replicate(nbclones, p, simplify=FALSE)), 1, prod), nm=ref.nn)
-	sapply(obs.names, function(nn) sum(prbs[ref.nn == nn]))
+predict.f.clones <- function(nbclones, p) {
+	pf <- matrix(p[cache.cat[[nbclones]]$comb], ncol=nbclones)
+	full.f <- apply(pf, 1, prod) * cache.cat[[nbclones]]$choose
+	f <- tapply(full.f, INDEX=cache.cat[[nbclones]]$names, sum)
+	
+	# Numerical errors are common
+	f[f<0] <- 0
+	f <- f/sum(f)
+	
+	ans <- setNames(rep(0, length(obs.names)), nm=obs.names)
+	ans[names(f)] <- f
+	ans
 }
 
-freq.model.random <- function(p, meanclones) {
-	distclone <- dpois(seq_len(max.nbclones), lambda=meanclones)
+predict.f <- function(lmuC, lpG, lpR, lpY) {
+	p <- logitp2p(lpG, lpR, lpY)
+	
+	distclone <- dpois(seq_len(max.nbclones), lambda=exp(lmuC))
 	distclone <- distclone / sum(distclone)
-	colSums(do.call(rbind, lapply(seq_len(max.nbclones), freq.model.exact, p=p))*distclone)
+	
+	colSums(do.call(rbind, lapply(seq_len(max.nbclones), predict.f.clones, p=p))*distclone)
 }
 
-mllik.exact <- function(x, obs, p)  
-	-dmultinom(obs, prob=freq.model.exact(p, nbclones=round(x)), log=TRUE)
-
-mllik.random <- function(x, obs, p) # x can be a vector
-	-dmultinom(obs, prob=freq.model.random(p, meanclones=x), log=TRUE)
-
-maxlik.exact.manual <- function(obs, p=obs2p(obs)$p.estimates, CI=0.95) {
-	nbcl <- seq_len(max.nbclones)
-	mll  <- sapply(nbcl, mllik.exact, obs=obs, p=p)
-	sup  <- mll - min(mll) + qchisq(CI, 1)/2
+maxlik.mle <- function(obs, CI=0.95) {
+	mll <- function(lmuC, lpG, lpR, lpY) {
+		print(exp(lmuC))
+		if (exp(lmuC) > max.nbclones) return (mll(log(max.nbclones), lpG, lpR, lpY) + 1000*(lmuC-log(max.nbclones)))
+		if (exp(lmuC) < 1)            return (mll(log(1), lpG, lpR, lpY) + 1/exp(lmuC)^2)
+		pred.f <- predict.f(lmuC, lpG, lpR, lpY)
+		-dmultinom(obs, prob=pred.f, log=TRUE)
+	}
+	
+	fit <- try(stats4::mle(mll, start=c(lmuC=1, lpG=0, lpR=0, lpY=-2)))
+	
+	if (class(fit) == "try-error") return(list(coef=NA, x=NA))
+	
+	# fit.ci <- confint(fit, level=CI)
+		
 	list(
-		x   = nbcl, 
-		mll = mll,
-		estim.x = nbcl[which.min(mll)],
-		CI      = range(nbcl[sup < 0])
-	)
+		coef     = fit@coef, 
+		x        = c(muC=unname(exp(fit@coef["lmuC"])), logitp2p(fit@coef["lpG"], fit@coef["lpR"], fit@coef["lpY"]))
+	) 
 }
-
-maxlik.random.manual <- function(obs, p=obs2p(obs)$p.estimates, CI=0.95, n=20) {
-	nbcl <- seq(0, max.nbclones, length=n+1)[-1]
-	mll  <- sapply(nbcl, mllik.random, obs=obs, p=p)
-	sup  <- mll - min(mll) - qchisq(CI, 1)/2
-	list(
-		x   = nbcl, 
-		mll = mll,
-		estim.x = nbcl[which.min(mll)],
-		CI      = range(nbcl[sup < 0])
-	)
-}
-
 
 plot.distcomp <- function(frq1, frq2, ...) {
 	barplot(rbind(frq1, frq2), beside=TRUE)
